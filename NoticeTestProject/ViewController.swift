706b6da8c247c094198e7cc21a93dbeda3fb3cc5
HyeongHwan <gudghks56@gmail.com>
2022-03-09 19:27:59 +0900

    Initial Commit

---
+++ NoticeTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoticeTestProject
+//
+//  Created by 박형환 on 2022/03/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
